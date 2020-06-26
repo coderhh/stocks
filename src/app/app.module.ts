8e06c293f9c24fa4d2c8ebdb631aa3b49c5a4088
coderhh <yehanghan@gmail.com>
2020-06-26 19:01:44 +0800

    init commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { StocksService } from "./services/stocks.service";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { SummaryComponent } from "./components/summary/summary.component";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { ManageComponent } from "./components/manage/manage.component";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SummaryComponent,
+    DashboardComponent,
+    ManageComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  providers: [StocksService],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
